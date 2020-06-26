6690035775fa70a6428c565796a0bf1d8020ef9e
AC <alphaconverter@protonmail.com>
2020-06-26 14:20:28 +0200

    add part 7

--- death_functions.py
+++ death_functions.py
@@ -1,16 +1,17 @@
 import tcod as libtcod
 
 from game_states import GameStates
+from game_messages import Message
 from render_functions import RenderOrder
 
 def kill_player(player):
     player.char = '%'
     player.color = libtcod.dark_red
 
-    return 'You died!', GameStates.PLAYER_DEAD
+    return Message('You died!', libtcod.red), GameStates.PLAYER_DEAD
 
 def kill_monster(monster):
-    death_message = '{0} is dead!'.format(monster.name.capitalize())
+    death_message = Message('{0} is dead!'.format(monster.name.capitalize()), libtcod.orange)
 
     monster.char = '%'
     monster.color = libtcod.dark_red
--- engine.py
+++ engine.py
@@ -4,6 +4,7 @@ import warnings # disable deprecation warnings ... (see also newer tutorial)
 
 from entity import Entity, get_blocking_entities_at_location
 from input_handlers import handle_keys
+from game_messages import MessageLog
 from game_states import GameStates
 from death_functions import kill_monster, kill_player
 from components.fighter import Fighter
@@ -14,10 +15,19 @@ from fov_functions import initialize_fov, recompute_fov
 warnings.simplefilter("ignore")
 
 def main():
-    screen_width = 48
-    screen_height = 27
-    map_width = 48
-    map_height = 22
+    screen_width = 64
+    screen_height = 36
+
+    bar_width = 20
+    panel_height = 7
+    panel_y = screen_height - panel_height
+
+    message_x = bar_width + 2
+    message_width = screen_width - bar_width - 2
+    message_height = panel_height - 1
+
+    map_width = screen_width
+    map_height = screen_height - panel_height
 
     room_max_size = 8
     room_min_size = 5
@@ -45,6 +55,7 @@ def main():
     libtcod.console_init_root(screen_width, screen_height, 'ATRL: A Tiny RogueLike', False)
 
     con = libtcod.console_new(screen_width, screen_height)
+    panel = libtcod.console_new(screen_width, panel_height)
 
     game_map = GameMap(map_width, map_height)
     game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities, max_monsters_per_room)
@@ -52,18 +63,20 @@ def main():
     fov_recompute = True
     fov_map = initialize_fov(game_map)
 
+    message_log = MessageLog(message_x, message_width, message_height)
+
     key = libtcod.Key()
     mouse = libtcod.Mouse()
 
     game_state = GameStates.PLAYERS_TURN
 
     while not libtcod.console_is_window_closed():
-        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
+        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS | libtcod.EVENT_MOUSE, key, mouse)
 
         if fov_recompute:
             recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
 
-        render_all(con, entities, player, game_map, fov_map, fov_recompute, screen_width, screen_height, colors)
+        render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log, screen_width, screen_height, bar_width, panel_height, panel_y, mouse, colors)
         fov_recompute = False
 
         libtcod.console_flush()
@@ -104,7 +117,7 @@ def main():
             dead_entity = player_turn_result.get('dead')
 
             if message:
-                print(message)
+                message_log.add_message(message)
 
             if dead_entity:
                 if dead_entity == player:
@@ -112,7 +125,7 @@ def main():
                 else:
                     message = kill_monster(dead_entity)
 
-                print(message)
+                message_log.add_message(message)
 
         if game_state == GameStates.ENEMY_TURN:
             for entity in entities:
@@ -124,7 +137,7 @@ def main():
                         dead_entity = enemy_turn_result.get('dead')
 
                         if message:
-                            print(message)
+                            message_log.add_message(message)
 
                         if dead_entity:
                             if dead_entity == player:
@@ -132,7 +145,7 @@ def main():
                             else:
                                 message = kill_monster(dead_entity)
 
-                            print(message)
+                            message_log.add_message(message)
 
                             if game_state == GameStates.PLAYER_DEAD:
                                 break
