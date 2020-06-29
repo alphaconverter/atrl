15874fdb95247bb86457cbace3e6c29bbf4bc9a7
AC <alphaconverter@protonmail.com>
2020-06-29 16:52:07 +0200

    add part 13

--- loader_functions/initialize_new_game.py
+++ loader_functions/initialize_new_game.py
@@ -3,7 +3,10 @@ import tcod as libtcod
 from components.fighter import Fighter
 from components.inventory import Inventory
 from components.level import Level
+from components.equipment import Equipment
+from components.equippable import Equippable
 from entity import Entity
+from equipment_slots import EquipmentSlots
 from game_messages import MessageLog
 from game_states import GameStates
 from map_objects.game_map import GameMap
@@ -70,11 +73,17 @@ def get_constants():
     return constants
 
 def get_game_variables(constants):
-    fighter_component = Fighter(hp=100, defense=1, power=4)
+    fighter_component = Fighter(hp=100, defense=1, power=2)
     inventory_component = Inventory(20)
     level_component = Level()
-    player = Entity(0, 0, '@', libtcod.dark_blue, 'Player', blocks=True, render_order=RenderOrder.ACTOR, fighter=fighter_component, inventory=inventory_component, level=level_component)
+    equipment_component = Equipment()
+    player = Entity(0, 0, '@', libtcod.dark_blue, 'Player', blocks=True, render_order=RenderOrder.ACTOR, fighter=fighter_component, inventory=inventory_component, level=level_component, equipment=equipment_component)
     entities = [player]
+
+    equippable_component = Equippable(EquipmentSlots.MAIN_HAND, power_bonus=2)
+    dagger = Entity(0, 0, '-', libtcod.sky, 'Dagger', equippable=equippable_component)
+    player.inventory.add_item(dagger)
+    player.equipment.toggle_equip(dagger)
 
     game_map = GameMap(constants['map_width'], constants['map_height'])
     game_map.make_map(constants['max_rooms'], constants['room_min_size'], constants['room_max_size'], constants['map_width'], constants['map_height'], player, entities)
--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -6,6 +6,8 @@ from components.ai import BasicMonster
 from components.stairs import Stairs
 from components.item import Item
 from components.fighter import Fighter
+from components.equipment import EquipmentSlots
+from components.equippable import Equippable
 from game_messages import Message
 from item_functions import cast_lightning, heal, cast_fireball, cast_confuse
 from item_functions import heal
@@ -125,6 +127,8 @@ class GameMap:
             'confusion_scroll': from_dungeon_level([[10, 2]], self.dungeon_level),
             'lightning_scroll': from_dungeon_level([[25, 4]], self.dungeon_level),
             'fireball_scroll': from_dungeon_level([[25, 6]], self.dungeon_level),
+            'sword': from_dungeon_level([[5, 4]], self.dungeon_level),
+            'shield': from_dungeon_level([[15, 8]], self.dungeon_level),
         }
 
         for i in range(number_of_monsters):
@@ -154,6 +158,12 @@ class GameMap:
                 if item_choice == 'healing_potion':
                     item_component = Item(use_function=heal, amount=40)
                     item = Entity(x, y, '!', libtcod.orange, 'Healing Potion', render_order=RenderOrder.ITEM, item=item_component)
+                elif item_choice == 'sword':
+                    equippable_component = Equippable(EquipmentSlots.MAIN_HAND, power_bonus=3)
+                    item = Entity(x, y, '/', libtcod.sky, 'Sword', equippable=equippable_component)
+                elif item_choice == 'shield':
+                    equippable_component = Equippable(EquipmentSlots.OFF_HAND, defense_bonus=1)
+                    item = Entity(x, y, '[', libtcod.darker_orange, 'Shield', equippable=equippable_component)
                 elif item_choice == 'fireball_scroll':
                     item_component = Item(use_function=cast_fireball, targeting=True, targeting_message=Message('Left-click a target tile for the fireball, or right-click to cancel.', libtcod.light_cyan), damage=25, radius=3)
                     item = Entity(x, y, '#', libtcod.red, 'Fireball Scroll', render_order=RenderOrder.ITEM, item=item_component)
