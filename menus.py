aff80a28fc4cd21ad085813b12027f6cb020031e
AC <alphaconverter@protonmail.com>
2020-06-29 13:48:47 +0200

    add part 10

--- menus.py
+++ menus.py
@@ -38,3 +38,14 @@ def inventory_menu(con, header, inventory, inventory_width, screen_width, screen
 
     menu(con, header, options, inventory_width, screen_width, screen_height)
 
+def main_menu(con, background_image, screen_width, screen_height):
+    libtcod.image_blit_2x(background_image, 0, 0, 0)
+
+    libtcod.console_set_default_foreground(0, libtcod.light_yellow)
+    libtcod.console_print_ex(0, int(screen_width / 2), int(screen_height / 2) - 10, libtcod.BKGND_NONE, libtcod.CENTER, 'ATRL: A Tiny RogueLike')
+    libtcod.console_print_ex(0, int(screen_width / 2), int(screen_height - 2), libtcod.BKGND_NONE, libtcod.CENTER, 'by alphaconverter')
+
+    menu(con, '', ['Play a new game', 'Continue last game', 'Quit'], 24, screen_width, screen_height)
+
+def message_box(con, header, width, screen_width, screen_height):
+    menu(con, header, [], width, screen_width, screen_height)
